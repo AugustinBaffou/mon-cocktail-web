63fd7dd33ee517ef8251e63cf64e0a2b84f437e8
AugustinBaffou <augustinbaffou@gmail.com>
2025-02-18 12:02:32 +0100

    Initial commit

---
+++ backend/src/main/java/com/augustinbaffou/mon_cocktail/MonCocktailApplication.java
@@ -0,0 +1,13 @@
+package com.augustinbaffou.mon_cocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonCocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonCocktailApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/augustinbaffou/mon_cocktail/MonCocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustinbaffou.mon_cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonCocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
