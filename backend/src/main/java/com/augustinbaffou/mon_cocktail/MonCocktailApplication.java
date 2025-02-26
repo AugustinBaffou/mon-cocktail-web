d78883ca3c6b9ca578cdbf8f797cdd5e850c6c44
AugustinBaffou <augustinbaffou@gmail.com>
2025-02-26 11:24:15 +0100

    User environnement variable and arrange properties

--- backend/src/main/java/com/augustinbaffou/mon_cocktail/MonCocktailApplication.java
+++ backend/src/main/java/com/augustinbaffou/mon_cocktail/MonCocktailApplication.java
@@ -2,12 +2,18 @@ package com.augustinbaffou.mon_cocktail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MonCocktailApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonCocktailApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // Charger les variables d'environnement depuis le fichier .env
+        Dotenv dotenv = Dotenv.configure().load();
+        
+        // Transférer les variables d'environnement du fichier .env vers System.properties
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        
+        SpringApplication.run(MonCocktailApplication.class, args);
+    }
 }
