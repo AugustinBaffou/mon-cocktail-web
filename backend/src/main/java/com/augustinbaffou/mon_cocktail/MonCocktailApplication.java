8931d2641075c96c4283ea2b087b802be0655449
AugustinBaffou <augustinb@live.fr>
2025-10-11 21:14:01 +0200

    ajout dockerfile et suppression de dotenv

--- backend/src/main/java/com/augustinbaffou/mon_cocktail/MonCocktailApplication.java
+++ backend/src/main/java/com/augustinbaffou/mon_cocktail/MonCocktailApplication.java
@@ -2,18 +2,11 @@ package com.augustinbaffou.mon_cocktail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MonCocktailApplication {
 
     public static void main(String[] args) {
-        // Charger les variables d'environnement depuis le fichier .env
-        Dotenv dotenv = Dotenv.configure().load();
-        
-        // Transférer les variables d'environnement du fichier .env vers System.properties
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
-        
         SpringApplication.run(MonCocktailApplication.class, args);
     }
 }
